f5938379b730bed564d0a91c41eb31c6d0b992bc
Matty G <meawoppl@gmail.com>
2018-02-10 16:51:00 -0800

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -1,1 +1,9 @@
-ansible-playbook -i "localhost," -c local helloworld.yml 
+// Install ansible
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
+
+// Use it to run the other stuff locally
+ansible-playbook -i "localhost," -c local orbit.yml 
