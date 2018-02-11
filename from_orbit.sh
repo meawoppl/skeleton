f9393917df3d3ba8684430cff4debe3e6a68275c
meawoppl <meawoppl@gmail.com>
2018-02-10 17:31:03 -0800

    Add indentation and git link

--- from_orbit.sh
+++ from_orbit.sh
@@ -5,8 +5,8 @@ sudo apt-get install -y ansible git software-properties-common
 mkdir -p ~/repos
 cd repos
 rm -rf skeleton
-git clone git@github.com:meawoppl/skeleton.git
+git clone https://github.com/meawoppl/skeleton.git
 cd skeleton
 
 # Use it to run the other stuff locally
-ansible-playbook -i "localhost," -c local orbit.yml 
+ansible-playbook --become -i "localhost," -c local orbit.yml 
